cbb202a50bbe5e5f0f2ad28076fd00323952726c
Thanathip Limna <boatkrap@gmail.com>
2015-08-25 17:11:03 +0700

    fix admin operation

--- nokkhumclient/admin/processor_operating.py
+++ nokkhumclient/admin/processor_operating.py
@@ -7,7 +7,7 @@ Created on Mar 1, 2013
 from .. import processor_operating
 from . import compute_nodes
 class ProcessorOperating(processor_operating.ProcessorOperating):
-    
+
     @property
     def compute_node(self):
         if 'compute_node' in self._info:
@@ -17,12 +17,12 @@ class ProcessorOperating(processor_operating.ProcessorOperating):
 
 class ProcessorOperatingManager(processor_operating.ProcessorOperatingManager):
     resource_class = ProcessorOperating
-    
+
     def get(self, processor_id):
         return self._get('/admin/processors/%s/operating'%str(processor_id), "processor_operating")
-    
+
     def update(self, processor, action):
         body = dict(
                     processor_operating=dict(action=action)
                     )
-        return self._update('/admin/cameras/%s/operating'%str(processor.id), "processor_operating", body)
+        return self._update('/admin/processors/%s/operating'%str(processor.id), "processor_operating", body)
